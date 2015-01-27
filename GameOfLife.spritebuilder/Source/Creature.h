20fe21ac4f3f7510e92d9945f0f566c0e3aae8b7
Maria Luisa <marialuisa@Marias-MacBook-Pro.local>
2015-01-26 23:13:09 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Creature.h
@@ -0,0 +1,13 @@
+//
+//  Creature.h
+//  GameOfLife
+//
+//  Created by Maria Luisa on 1/26/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Creature : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/Source/Creature.m
@@ -0,0 +1,13 @@
+//
+//  Creature.m
+//  GameOfLife
+//
+//  Created by Maria Luisa on 1/26/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Creature.h"
+
+@implementation Creature
+
+@end
